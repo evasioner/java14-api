d1f72077e3c6d325cf02ceee8d9452303c559fc6
hyunsoo.lee <hyunsoo.lee@way2bit.com>
2020-06-07 12:59:59 +0900

    first commit

---
+++ src/test/java/newapi/newapi/NewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package newapi.newapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
